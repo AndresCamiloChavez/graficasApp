e0acedd9b7caa08ef0a6baccaba40808a89ac130
ANDRESCZ <andres.cz.chavez@gmail.com>
2022-07-21 21:20:55 -0500

    Firts Commit

---
+++ src/app/graficas/pages/graficas-doble/graficas-doble.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graficas-doble',
+  templateUrl: './graficas-doble.component.html',
+  styleUrls: ['./graficas-doble.component.css']
+})
+export class GraficasDobleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
