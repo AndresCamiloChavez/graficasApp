1823a7a836ae1314cf85bfef292d7c0dbb81c3a7
ANDRESCZ <andres.cz.chavez@gmail.com>
2022-07-27 20:18:49 -0500

    Gráfica de DONA

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonaComponent implements OnInit {
 
-  constructor() { }
 
   ngOnInit(): void {
+    
   }
+// Doughnut
+public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+public doughnutChartData: ChartData<'doughnut'> = {
+  labels: this.doughnutChartLabels,
+  datasets: [
+    { data: [ 350, 450, 100 ],},
+  ]
+};
+public doughnutChartType: ChartType = 'doughnut';
+
+// events
+public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+  console.log(event, active);
+}
+
+public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+  console.log(event, active);
+}
 
 }
